4650f45730fc3ed99fbfbae1dfc9bec4d0085a23
rugyada <46781309+rugyada@users.noreply.github.com>
2022-09-22 15:18:25 +0200

    Update org.openmandriva.plasma.desktop.defaultPanel-layout.js

--- nn/desktops/Plasma/org.openmandriva.plasma.desktop.defaultPanel-layout.js
+++ nn/desktops/Plasma/org.openmandriva.plasma.desktop.defaultPanel-layout.js
@@ -22,7 +22,8 @@ if (panelIds.length == 1) {
 }
 
 // let's calculate desired panel height based on scren's DPI
-panel.height = gridUnit * 3
+// panel.height = gridUnit * 3
+panel.height = 2 * Math.floor(gridUnit * 2.5 / 2)
 panel.alignment = "left";
 panel.hiding = "none";
 
@@ -31,7 +32,6 @@ var launcher = panel.addWidget("org.kde.plasma.kicker")
 launcher.currentConfigGroup = ["Shortcuts"]
 launcher.writeConfig("global", "Alt+F1")
 launcher.currentConfigGroup = ["General"]
-launcher.writeConfig("favorites", "preferred://browser,org.kde.kmail.desktop,kcm_kdeconnect.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.kde.konsole.desktop,systemsettings.desktop")
 launcher.writeConfig("limitDepth", "false")
 launcher.writeConfig("useExtraRunners", "true")
 launcher.writeConfig("alignResultsToBottom", "true")
@@ -41,25 +41,9 @@ launcher.writeConfig("showRecentApps", "true")
 launcher.writeConfig("showRecentDocs", "true")
 launcher.writeConfig("showIconsRootLevel", "true")
 
-var tasks = panel.addWidget("org.kde.plasma.taskmanager")
-tasks.currentConfigGroup = ["General"]
-tasks.writeConfig("launchers","")
-tasks.writeConfig("forceStripes","true")
-tasks.writeConfig("middleClickAction", "Close")
-tasks.writeConfig("onlyGroupWhenFull","true")
-tasks.writeConfig("groupingStrategy","1")
-tasks.writeConfig("highlightWindows","false")
-tasks.writeConfig("maxStripes","1")
-tasks.writeConfig("showOnlyCurrentDesktop","true")
-tasks.writeConfig("showOnlyCurrentScreen","false")
-tasks.writeConfig("showOnlyMinimized","false")
-tasks.writeConfig("showToolTips","false")
-tasks.writeConfig("sortingStrategy","2")
-
-var pager = panel.addWidget("org.kde.plasma.pager")
-pager.currentConfigGroup = ["General"]
-pager.writeConfig("showWindowIcons","true")
-pager.writeConfig("displayedText", "Number")
+panel.addWidget("org.kde.plasma.taskmanager")
+// panel.addWidget("org.kde.plasma.icontasks")
+panel.addWidget("org.kde.plasma.pager")
 
 /* Next up is determining whether to add the Input Method Panel
  * widget to the panel or not. This is done based on whether
@@ -101,29 +85,13 @@ var langIds = ["as",    // Assamese
                "zh_TW"] // Traditional Chinese
 
 if (langIds.indexOf(languageId) != -1) {
-	panel.addWidget("org.kde.plasma.kimpanel");
+    panel.addWidget("org.kde.plasma.kimpanel");
 }
 
-var systray = panel.addWidget("org.kde.plasma.systemtray")
-systray.currentConfigGroup = ["General"]
-systray.writeConfig("communicationsShow", "true")
-systray.writeConfig("applicationStatusShown","true")
-systray.writeConfig("ShowCommunications","true")
-systray.writeConfig("systemServicesShown","true")
-systray.writeConfig("hardwareControlShown","true")
-systray.writeConfig("miscellaneousShown","true")
-systray.writeConfig("extraItems", "org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.clipboard")
-systray.writeConfig("hiddenItems", "hp-systray,python3.4m")
-systray.writeConfig("knownItems", "org.kde.plasma.notifications,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.battery,org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.clipboard")
+panel.addWidget("org.kde.plasma.systemtray")
+// systray.writeConfig("hiddenItems", "hp-systray,python3.4m")
 
-var clock = panel.addWidget("org.kde.plasma.digitalclock")
-clock.currentConfigGroup = ["Appearance"]
-clock.writeConfig("showDate","true")
-clock.writeConfig("showWeekNumbers","true")
-clock.writeConfig("dateFormat", "isoDate")
-clock.writeConfig("use24hFormat", "2")
-clock.reloadConfig()
-
+panel.addWidget("org.kde.plasma.digitalclock")
 panel.addWidget("org.kde.plasma.trash")
 
 sleep(0.5)
